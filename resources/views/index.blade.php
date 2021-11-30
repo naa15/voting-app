4eeffedcfbc7974613698616ea7a32ad9e24b8c0
naa15 <naltu15@freeuni.edu.ge>
2021-12-01 00:14:33 +0400

    style: add different idea type designs

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -119,6 +119,175 @@
             </div>
         </div>
         <!--- end single idea-container --->
+
+        <div
+            class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer">
+            <div class="border-r border-gray-100 px-5 py-8">
+                <div class="text-center">
+                    <div class="font-semibold text-2xl text-blue">66</div>
+                    <div class="text-gray-500">Votes</div>
+                </div>
+
+                <div class="mt-8">
+                    <button
+                        class="w-20 bg-blue border border-gray-200 hover:border-blue-hover font-bold text-white 
+                        text-xxs uppercase rounded-xl transition duration-150 ease-in px-4 py-3">
+                        Voted
+                    </button>
+                </div>
+            </div>
+
+            <div class="flex flex-1 px-2 py-6">
+                <div class="flex-none">
+                    <a href="#">
+                        <img src="https://source.unsplash.com/200x200/?face&crop=face&v=2" alt="avatar"
+                            class="w-14 h-14 rounded-xl">
+                    </a>
+                </div>
+                <div class="w-full mx-4">
+                    <h4 class="text-xl font-semibold">
+                        <a href="#" class="hover:underline">A random title can go here</a>
+                    </h4>
+
+                    <div class="text-gray-600 mt-3 line-clamp-3">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae assumenda consequuntur id
+                        itaque nostrum, suscipit amet, repudiandae dolorem aperiam autem ipsum fugiat exercitationem in
+                        labore sapiente magni. Numquam, modi neque? Repudiandae deserunt a sed laborum vitae quasi sint,
+                        est unde iure in dolorem nam esse dolores provident assumenda maiores mollitia necessitatibus,
+                        ipsum veritatis aut ducimus odit repellendus quisquam blanditiis? Voluptas consectetur fugiat
+                        molestias, culpa voluptatibus, ut suscipit at ratione quod expedita deserunt aut ipsam iure
+                        assumenda, nulla provident adipisci libero incidunt corrupti laudantium rerum? Expedita
+                        doloribus aliquid sint itaque illo. Officiis animi corrupti quaerat, et hic tempora aliquid
+                        pariatur voluptatibus.
+                    </div>
+
+                    <div class="flex items-center justify-between mt-6">
+                        <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
+                            <div>10 hours ago</div>
+                            <div>&bull;</div>
+                            <div>Category 1</div>
+                            <div>&bull;</div>
+                            <div class="text-gray-900">3 Comments</div>
+                        </div>
+
+                        <div x-data="{ isOpen: false }" class="flex items-center space-x-2">
+                            <div
+                                class="bg-yellow text-xxs text-white font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">
+                                In Progress
+                            </div>
+
+                            <button @click="isOpen = !isOpen"
+                                class="relative bg-gray-100 hover:bg-gray-200 border outline-none rounded-full h-7 transition duration-150 ease-in py-2 px-3">
+                                <svg fill="currentColor" width="24" height="6">
+                                    <path
+                                        d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z"
+                                        style="color: rgba(163, 163, 163, .5)">
+                                </svg>
+                                <ul x-cloak x-show="isOpen" x-transition.origin.top.left @click.away="isOpen = false"
+                                    @keydown.escape.window="isOpen = false"
+                                    class="absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 ml-8">
+                                    <li><a href="#"
+                                            class="hover:bg-gray-100 block transition duration-150 
+                                    ease-in px-5 py-3">Mark
+                                            as Spam</a></li>
+                                    <li></li>
+                                    <li><a href="#"
+                                            class="hover:bg-gray-100 block transition duration-150 
+                                    ease-in px-5 py-3">Delete</a>
+                                    </li>
+                                    <li></li>
+                                </ul>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> <!--- end single idea-container --->
+
+        <div
+            class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer">
+            <div class="border-r border-gray-100 px-5 py-8">
+                <div class="text-center">
+                    <div class="font-semibold text-2xl">0</div>
+                    <div class="text-gray-500">Votes</div>
+                </div>
+
+                <div class="mt-8">
+                    <button
+                        class="w-20 bg-gray-200  border border-gray-200 hover:border-gray-400 font-bold 
+                            text-xxs uppercase rounded-xl transition duration-150 ease-in px-4 py-3">
+                        Vote
+                    </button>
+                </div>
+            </div>
+
+            <div class="flex flex-1 px-2 py-6">
+                <div class="flex-none">
+                    <a href="#">
+                        <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar"
+                            class="w-14 h-14 rounded-xl">
+                    </a>
+                </div>
+                <div class="w-full mx-4">
+                    <h4 class="text-xl font-semibold">
+                        <a href="#" class="hover:underline">A random title can go here</a>
+                    </h4>
+
+                    <div class="text-gray-600 mt-3 line-clamp-3">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae assumenda consequuntur id
+                        itaque nostrum, suscipit amet, repudiandae dolorem aperiam autem ipsum fugiat exercitationem in
+                        labore sapiente magni. Numquam, modi neque? Repudiandae deserunt a sed laborum vitae quasi sint,
+                        est unde iure in dolorem nam esse dolores provident assumenda maiores mollitia necessitatibus,
+                        ipsum veritatis aut ducimus odit repellendus quisquam blanditiis? Voluptas consectetur fugiat
+                        molestias, culpa voluptatibus, ut suscipit at ratione quod expedita deserunt aut ipsam iure
+                        assumenda, nulla provident adipisci libero incidunt corrupti laudantium rerum? Expedita
+                        doloribus aliquid sint itaque illo. Officiis animi corrupti quaerat, et hic tempora aliquid
+                        pariatur voluptatibus.
+                    </div>
+
+                    <div class="flex items-center justify-between mt-6">
+                        <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
+                            <div>10 hours ago</div>
+                            <div>&bull;</div>
+                            <div>Category 1</div>
+                            <div>&bull;</div>
+                            <div class="text-gray-900">3 Comments</div>
+                        </div>
+
+                        <div x-data="{ isOpen: false }" class="flex items-center space-x-2">
+                            <div
+                                class="bg-red text-xxs text-white font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">
+                                Closed
+                            </div>
+
+                            <button @click="isOpen = !isOpen"
+                                class="relative bg-gray-100 hover:bg-gray-200 border outline-none rounded-full h-7 transition duration-150 ease-in py-2 px-3">
+                                <svg fill="currentColor" width="24" height="6">
+                                    <path
+                                        d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z"
+                                        style="color: rgba(163, 163, 163, .5)">
+                                </svg>
+                                <ul x-cloak x-show="isOpen" x-transition.origin.top.left @click.away="isOpen = false"
+                                    @keydown.escape.window="isOpen = false"
+                                    class="absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 ml-8">
+                                    <li><a href="#"
+                                            class="hover:bg-gray-100 block transition duration-150 
+                                        ease-in px-5 py-3">Mark
+                                            as Spam</a></li>
+                                    <li></li>
+                                    <li><a href="#"
+                                            class="hover:bg-gray-100 block transition duration-150 
+                                        ease-in px-5 py-3">Delete</a>
+                                    </li>
+                                    <li></li>
+                                </ul>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {{-- end single idea-container --}}
     </div>
     <!--- end ideas container --->
 </x-app-layout>
