197bb267076a24c57036dd08faeb0137c3ecbd9b
naa15 <naltu15@freeuni.edu.ge>
2022-01-13 15:15:45 +0400

    feat: category filters

--- app/Http/Livewire/StatusFilters.php
+++ app/Http/Livewire/StatusFilters.php
@@ -8,35 +8,32 @@ use App\Models\Status;
 
 class StatusFilters extends Component
 {
-	public $status = 'All';
+	public $status;
 
 	public $statusCount;
-
-	protected $queryString = [
-		'status',
-	];
 
 	public function mount()
 	{
 		$this->statusCount = Status::getCount();
+		$this->status = request()->status ?? 'All';
 
 		if (Route::currentRouteName() == 'idea.show')
 		{
 			$this->status = null;
-			$this->queryString = [];
 		}
 	}
 
 	public function setStatus($newStatus)
 	{
 		$this->status = $newStatus;
+		$this->emit('queryStringUpdatedStatus', $this->status);
 
-		// if ($this->getPreviousRouteName() == 'idea.show')
-		// {
-		return redirect()->route('idea.index', [
-			'status' => $this->status,
-		]);
-		// }
+		if ($this->getPreviousRouteName() == 'idea.show')
+		{
+			return redirect()->route('idea.index', [
+				'status' => $this->status,
+			]);
+		}
 	}
 
 	public function render()
