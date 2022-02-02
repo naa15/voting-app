d49b6d40960ef78dd50487554c6c1b28fe8c4c3f
naa15 <naltu15@freeuni.edu.ge>
2022-02-02 17:32:16 +0400

    style: notifications styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,14 +27,178 @@
             @if (Route::has('login'))
                 <div class="px-6 py-4">
                     @auth
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
+                        <div class="flex items-center space-x-4">
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                                            this.closest('form').submit();">
+                                    {{ __('Log Out') }}
+                                </a>
+                            </form>
 
-                            <a href="{{ route('logout') }}" onclick="event.preventDefault(); 
-                                                                    this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </a>
-                        </form>
+                            <div x-data="{ isOpen : false }" class="relative">
+                                <button @click=" isOpen = !isOpen ">
+                                    <svg class="h-8 w-8 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                        <path
+                                            d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z" />
+                                    </svg>
+                                    <div class="absolute rounded-full bg-red text-white text-xxs w-6 h-6 flex justify-center items-center border-2 -top-1 -right-1">
+                                        8
+                                    </div>
+                                </button>
+                                <ul x-show="isOpen" 
+                                    x-transition.origin.top 
+                                    @click.away="isOpen = false" 
+                                    x-cloak
+                                    @keydown.escape.window="isOpen = false"
+                                    class="absolute w-76 md:w-96 max-h-128 overflow-y-auto text-left bg-white shadow-dialog text-sm rounded-xl
+                                    z-10 -right-28 md:-right-12"
+                                >
+                                    <li><a href="#" @click.prevent="
+                                                        isOpen = false
+                                                    "
+                                            class="flex hover:bg-gray-100 transition duration-150
+                                                ease-in px-5 py-3">
+                                            <img src="{{ auth()->user()->getAvatar() }}" class="rounded-xl w-10 h-10"
+                                                alt="avatar">
+                                            <div class="ml-4">
+                                                <div>
+                                                    <span class="font-semibold">
+                                                        gelatavadze
+                                                    </span>
+                                                    commented on
+                                                    <span class="font-semibold">
+                                                        This is my idea
+                                                    </span>
+                                                    :
+                                                    <span>
+                                                        "Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                                        Perferendis, ducimus."
+                                                    </span>
+                                                </div>
+                                                <div class="text-xs text-gray-500 mt-2">1 hour ago</div>
+                                            </div>
+                                        </a>
+                                    </li>
+
+                                    <li><a href="#" @click.prevent="
+                                                isOpen = false
+                                                "
+                                            class="flex hover:bg-gray-100 transition duration-150
+                                            ease-in px-5 py-3">
+                                            <img src="{{ auth()->user()->getAvatar() }}" class="rounded-xl w-10 h-10"
+                                                alt="avatar">
+                                            <div class="ml-4">
+                                                <div>
+                                                    <span class="font-semibold">
+                                                        gelatavadze
+                                                    </span>
+                                                    commented on
+                                                    <span class="font-semibold">
+                                                        This is my idea
+                                                    </span>
+                                                    :
+                                                    <span>
+                                                        "Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                                        Perferendis, ducimus."
+                                                    </span>
+                                                </div>
+                                                <div class="text-xs text-gray-500 mt-2">1 hour ago</div>
+                                            </div>
+                                        </a>
+                                    </li>
+
+                                    <li><a href="#" @click.prevent="
+                                            isOpen = false
+                                            "
+                                            class="flex hover:bg-gray-100 transition duration-150
+                                        ease-in px-5 py-3">
+                                            <img src="{{ auth()->user()->getAvatar() }}" class="rounded-xl w-10 h-10"
+                                                alt="avatar">
+                                            <div class="ml-4">
+                                                <div>
+                                                    <span class="font-semibold">
+                                                        gelatavadze
+                                                    </span>
+                                                    commented on
+                                                    <span class="font-semibold">
+                                                        This is my idea
+                                                    </span>
+                                                    :
+                                                    <span>
+                                                        "Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                                        Perferendis, ducimus."
+                                                    </span>
+                                                </div>
+                                                <div class="text-xs text-gray-500 mt-2">1 hour ago</div>
+                                            </div>
+                                        </a>
+                                    </li>
+
+                                    <li><a href="#" @click.prevent="
+                                            isOpen = false
+                                            "
+                                            class="flex hover:bg-gray-100 transition duration-150
+                                        ease-in px-5 py-3">
+                                            <img src="{{ auth()->user()->getAvatar() }}" class="rounded-xl w-10 h-10"
+                                                alt="avatar">
+                                            <div class="ml-4">
+                                                <div>
+                                                    <span class="font-semibold">
+                                                        gelatavadze
+                                                    </span>
+                                                    commented on
+                                                    <span class="font-semibold">
+                                                        This is my idea
+                                                    </span>
+                                                    :
+                                                    <span>
+                                                        "Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                                        Perferendis, ducimus."
+                                                    </span>
+                                                </div>
+                                                <div class="text-xs text-gray-500 mt-2">1 hour ago</div>
+                                            </div>
+                                        </a>
+                                    </li>
+
+                                    <li><a href="#" @click.prevent="
+                                            isOpen = false
+                                            "
+                                            class="flex hover:bg-gray-100 transition duration-150
+                                        ease-in px-5 py-3">
+                                            <img src="{{ auth()->user()->getAvatar() }}" class="rounded-xl w-10 h-10"
+                                                alt="avatar">
+                                            <div class="ml-4">
+                                                <div class="line-clamp-6">
+                                                    <span class="font-semibold">
+                                                        gelatavadze
+                                                    </span>
+                                                    commented on
+                                                    <span class="font-semibold">
+                                                        This is my idea
+                                                    </span>
+                                                    :
+                                                    <span>
+                                                        "Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                                        Perferendis, ducimus."
+                                                    </span>
+                                                </div>
+                                                <div class="text-xs text-gray-500 mt-2">1 hour ago</div>
+                                            </div>
+                                        </a>
+                                    </li>
+
+                                    <li class="border-t border-gray-300 text-center">
+                                        <button href="#"
+                                            class="block w-full font-semibold hover:bg-gray-100 transition duration-150
+                                            ease-in px-5 py-4">
+                                            Mark all as read
+                                        </button>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log
                             in</a>
@@ -93,7 +257,7 @@
         </div>
         <div class="w-full px-2 md:px-0 md:w-175">
             <livewire:status-filters />
-            
+
             <div class="mt-8">
                 {{ $slot }}
             </div>
